e11c26fc4a9910c7ce81e06a5e5bc8d16653a750
Anna-K-S <anna.sergeevna1622@gmail.com>
2022-12-09 01:02:10 +0300

    Error Types in Python

---
+++ errors_type.py
@@ -0,0 +1,28 @@
+# SyntaxError
+
+# des print():   #'def'
+# pass
+
+# NameError
+
+# print(x)
+
+# TypeError
+
+# len(1) # у целого числа нет длины
+
+# IndexError
+
+# my_list = [1, 2, 3]
+# print(my_list[3]) # максимальный индекс 2
+
+# ValueError
+
+# print(int('Hi')) # Ошибка значения
+
+# KeyError
+# my_dict = {'1': 'first', '2': 'second'}
+# my_dict['third']
+
+# AttributeError
+# 'red'.artr()
