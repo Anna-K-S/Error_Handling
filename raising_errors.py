3b011efa30c6e54a070f836e0745f17f1005a847
Anna-K-S <anna.sergeevna1622@gmail.com>
2022-12-09 01:30:27 +0300

    Error Types in Python

---
+++ raising_errors.py
@@ -0,0 +1,63 @@
+# raise ValueError ('My invalid value')
+# raise ValueError()
+
+def get_rainbow_color(color_number):
+    color_number_list = [1, 2, 3, 4, 5, 6, 7]
+    if type(color_number) is not int:
+        raise TypeError('Color number must be integer type')
+    if color_number not in color_number_list:
+        raise ValueError('Color number must be in range of integer from 1 to 7')
+    if color_number == 1:
+        return 'red'
+    elif color_number == 2:
+        return 'orange'
+    elif color_number == 3:
+        return 'yellow'
+    elif color_number == 4:
+        return 'green'
+    elif color_number == 5:
+        return 'blue'
+    elif color_number == 6:
+        return 'indigo'
+    elif color_number == 7:
+        return 'violet'
+
+
+# ValueError
+# color = get_rainbow_color(9)
+# TypeError
+# color_1 = get_rainbow_color('Hi')
+# print(color)
+
+def colorize_text(color_number1, text):
+    """
+
+    :param text: "Text" must be string type
+    :param color_number1: Color number must be integer type and must be in range
+     of integer from 1 to 7
+    :return:
+    """
+    color_number_list = [1, 2, 3, 4, 5, 6, 7]
+    if type(color_number1) is not int:
+        raise TypeError('Color number must be integer type')
+    if color_number1 not in color_number_list:
+        raise ValueError('Color number must be in range of integer from 1 to 7')
+    if type(text) is not str:
+        raise TypeError('"Text" must be string type')
+    if color_number1 == 1:
+        print('red ' + text)
+    elif color_number1 == 2:
+        print('orange' + text)
+    elif color_number1 == 3:
+        print('yellow ' + text)
+    elif color_number1 == 4:
+        print('green ' + text)
+    elif color_number1 == 5:
+        print('blue' + text)
+    elif color_number1 == 6:
+        print('indigo ' + text)
+    elif color_number1 == 7:
+        print('violet ' + text)
+
+
+# colorize_text(4, False)
